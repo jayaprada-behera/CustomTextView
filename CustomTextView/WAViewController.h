d3a910f1871902ccf14e2562426be729dbd986d6
Jayaprada Behera <jayaprada.be@gmail.com>
2014-03-03 00:47:57 +0530

    CustomTextView
    A UITextView With fixed number of lines

---
+++ CustomTextView/WAViewController.h
@@ -0,0 +1,14 @@
+//
+//  WAViewController.h
+//  CustomTextView
+//
+//  Created by Jayaprada Behera on 25/02/14.
+//  Copyright (c) 2014 Webileapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAViewController : UIViewController
+@property(nonatomic,strong)IBOutlet UILabel *textLbl;
+-(IBAction)clearText:(id)sender;
+@end
---
+++ CustomTextView/WAViewController.m
@@ -0,0 +1,90 @@
+//
+//  WAViewController.m
+//  CustomTextView
+//
+//  Created by Jayaprada Behera on 25/02/14.
+//  Copyright (c) 2014 Webileapps. All rights reserved.
+//
+
+#import "WAViewController.h"
+
+#define FONT_SIZE    21.f
+
+#define NSLineSeparatorCharacter           0x2028
+@interface WAViewController ()<UITextViewDelegate>
+{
+    UITextView *textView_;
+    float font;
+}
+@end
+
+@implementation WAViewController
+@synthesize textLbl;
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    textView_ = [[UITextView alloc]initWithFrame:CGRectMake(20, 42, 200, 200)];
+    textView_.delegate = self;
+    textView_.autocorrectionType = UITextAutocorrectionTypeNo;
+    textView_.font = [UIFont systemFontOfSize:FONT_SIZE];
+    textView_.dataDetectorTypes = UIDataDetectorTypeAll;
+    textView_.textContainer.maximumNumberOfLines = 9;
+    textView_.backgroundColor = [UIColor greenColor];
+    textView_.textContainer.lineBreakMode = NSLineBreakByWordWrapping;
+    
+    [self.view addSubview:textView_];
+    
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    UITextPosition *beginning = textView.beginningOfDocument; //Error=: request for member 'beginningOfDocument' in something not a structure or union
+    
+    UITextPosition *start = [textView positionFromPosition:beginning offset:range.location];
+    UITextPosition *end = [textView positionFromPosition:start offset:range.length];
+    UITextRange *textRange = [textView textRangeFromPosition:start toPosition:end];
+    
+    CGPoint cursorPosition = [textView caretRectForPosition:textRange.start].origin;
+    
+    //    NSLog(@"x%f,y%f",cursorPosition.x,cursorPosition.y);
+    
+    if ([text isEqualToString:@""]) {
+        //increase the font size  ,if deleting some text that come to certain posotion
+        
+        if (cursorPosition.x >170.f && cursorPosition.y > 100 && cursorPosition.y <160) {
+            if (font < FONT_SIZE) {
+                NSLog(@"delete,font increased");
+                font = textView.font.pointSize +2;
+                textView.font = [UIFont systemFontOfSize:font];
+            }
+        }
+    }else{
+        if (cursorPosition.x >170.f && cursorPosition.y > 150 ) {//at most end point of last line
+            //decrease the font size  ,if deleting some text that come to certain posotion
+            if (font < FONT_SIZE) {
+                NSLog(@"delete,font decreased");
+                font = textView.font.pointSize - 2;
+                textView.font = [UIFont systemFontOfSize:font];
+            }
+        }
+    }
+    //    if (cursorPosition.x == 0) {
+    //        textView.selectedRange = NSMakeRange(textView.text.length -0, 0);
+    ////       textView_.scrollEnabled = NO;
+    //        return NO;
+    //    }
+    return YES ;
+    
+}
+-(IBAction)clearText:(id)sender{
+    textView_.font = [UIFont systemFontOfSize:FONT_SIZE];
+    textView_.text = @"";
+}
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
